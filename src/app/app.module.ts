415388b7ec6dddcf4ef218f186d3bfd30f5f0c33
Vengeance00 <vengeance801@gmail.com>
2023-03-31 17:17:53 -0600

    Version 0.01 del frontend de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { IndexComponent } from './components/index/index.component';
+import { CardComponent } from './components/card/card.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IndexComponent,
+    CardComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
